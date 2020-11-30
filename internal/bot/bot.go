d6ab3e4accdbece21e9cb05afa4469f6f7eaccc3
Tom <tom@tdpain.net>
2020-11-30 22:51:29 +0000

    [internal/bot] Create

---
+++ internal/bot/bot.go
@@ -0,0 +1,36 @@
+package bot
+
+import (
+	"context"
+	"github.com/skwair/harmony"
+	"github.com/skwair/harmony/embed"
+)
+
+type Bot struct {
+	Client *harmony.Client
+	Prefix string
+}
+
+func New(client *harmony.Client, prefix string) *Bot {
+	b := new(Bot)
+	b.Client = client
+	b.Prefix = prefix
+
+	return b
+}
+
+func (b *Bot) SendMessage(channelID, content string) (*harmony.Message, error) {
+	return b.Client.Channel(channelID).SendMessage(context.Background(), content)
+}
+
+func (b *Bot) SendEmbed(channelID string, e *embed.Embed) (*harmony.Message, error) {
+	return b.Client.Channel(channelID).Send(context.Background(), harmony.WithEmbed(e))
+}
+
+func (b *Bot) IsSelf(id string) (bool, error) {
+	botUser, err := b.Client.CurrentUser().Get(context.Background())
+	if err != nil {
+		return false, err
+	}
+	return id == botUser.ID, nil
+}
