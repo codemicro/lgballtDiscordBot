ed8f884781bf0693306ca824447b288409b444eb
Tom <tom@tdpain.net>
2021-01-02 14:43:40 +0000

    Add build scripts

---
+++ scripts/build.bat
@@ -0,0 +1,4 @@
+@echo off
+
+go generate github.com/codemicro/lgballtDiscordBot/...
+go build github.com/codemicro/lgballtDiscordBot/cmd/lgballtDiscordBot
---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+go generate github.com/codemicro/lgballtDiscordBot/...
+go build github.com/codemicro/lgballtDiscordBot/cmd/lgballtDiscordBot
