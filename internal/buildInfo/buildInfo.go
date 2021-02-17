b69747645051054b7d7c0be69e17d44f00642e02
Tom <tom@tdpain.net>
2021-02-17 20:03:04 +0000

    Update for Go 1.16

--- internal/buildInfo/buildInfo.go
+++ internal/buildInfo/buildInfo.go
@@ -1,18 +1,62 @@
 package buildInfo
 
 import (
-    "runtime"
-    "time"
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"runtime"
+	"strings"
+	"time"
 )
 
-const (
-    Version = "3.3.0"
-    BuildDate = "16/02/2021 at 19:53:52"
-    LinesOfCode = "3233"
-    NumFiles = "50"
-)
+const Version = "3.3.0"
+
+//go:embed currentDate
+var BuildDate string
+//go:embed jdat
+var jdat []byte
 
 var (
-    GoVersion = runtime.Version()
-    StartTime = time.Now()
+	GoVersion   = runtime.Version()
+	StartTime   = time.Now()
+	LinesOfCode = "unknown"
+	NumFiles    = "unknown"
 )
+
+// https://github.com/hhatto/gocloc/blob/ecf2a9b510f6583a05c67b8705e9bd79e8015ce1/json.go#L3-L6
+type JSONLanguagesResult struct {
+	Languages []ClocLanguage `json:"languages"`
+	Total     ClocLanguage   `json:"total"`
+}
+
+// https://github.com/hhatto/gocloc/blob/ecf2a9b510f6583a05c67b8705e9bd79e8015ce1/language.go#L19-L25
+type ClocLanguage struct {
+	Name       string `json:"name,omitempty"`
+	FilesCount int32  `json:"files"`
+	Code       int32  `json:"code"`
+	Comments   int32  `json:"comment"`
+	Blanks     int32  `json:"blank"`
+}
+
+func init() {
+	var jlr JSONLanguagesResult
+
+	err := json.Unmarshal(jdat, &jlr)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, x := range jlr.Languages {
+		if strings.EqualFold(x.Name, "Go") {
+			LinesOfCode = fmt.Sprint(x.Code)
+			NumFiles = fmt.Sprint(x.FilesCount)
+			break
+		}
+	}
+
+    if BuildDate == "" {
+        BuildDate = "unknown"
+    } else {
+        BuildDate = strings.Trim(BuildDate, "\n")
+    }
+}
