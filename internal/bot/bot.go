189f1c3cd7ed016e551b0e9aa102d2336e3ad610
Tom <tom@tdpain.net>
2021-03-08 20:47:16 +0000

    Add basis of bot package

---
+++ internal/bot/bot.go
@@ -0,0 +1,78 @@
+package bot
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/codemicro/dgo-toolkit/route"
+	"github.com/codemicro/lgballtDiscordBot/internal/bot/components/misc"
+	"github.com/codemicro/lgballtDiscordBot/internal/buildInfo"
+	"github.com/codemicro/lgballtDiscordBot/internal/config"
+	"github.com/codemicro/lgballtDiscordBot/internal/logging"
+	"github.com/codemicro/lgballtDiscordBot/internal/state"
+	"time"
+)
+
+func Start(state *state.State) error {
+
+	session, err := discordgo.New("Bot " + config.Token)
+	if err != nil {
+		return err
+	}
+
+	kit := route.NewKit(session, []string{config.Prefix})
+
+	kit.ErrorHandler = func(err error) { logging.Error(err)	}
+
+	err = registerHandlers(kit, state)
+	if err != nil {
+		return err
+	}
+
+	err = session.Open()
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		f := func(text string) {
+			err := session.UpdateGameStatus(0, text)
+			if err != nil {
+				logging.Error(err)
+			}
+		}
+
+		if len(config.Statuses) == 1 {
+			f(fmt.Sprintf(config.Statuses[0], buildInfo.Version))
+			return
+		}
+
+		for {
+			for _, text := range config.Statuses {
+				f(fmt.Sprintf(text, buildInfo.Version))
+				time.Sleep(time.Second * 15)
+			}
+		}
+	}()
+
+	go func() {
+		state.WaitUntilShutdownTrigger()
+		_ = session.Close()
+		state.FinishGoroutine()
+	}()
+
+	return nil
+}
+
+func registerHandlers(kit *route.Kit, st *state.State) error {
+
+	// TODO: commands/reactions
+
+	err := misc.Init(kit, st)
+	if err != nil {
+		return err
+	}
+
+	kit.CreateHandlers()
+
+	return nil
+}
