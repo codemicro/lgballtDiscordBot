b7c010bc0cd23b31c2603df4efd256700288da00
Tom <tom@tdpain.net>
2021-03-26 00:42:39 +0000

    Add gocloc to InstallDeps

--- go.mod
+++ go.mod
@@ -5,13 +5,13 @@ go 1.16
 require (
 	github.com/PuerkitoBio/goquery v1.6.0
 	github.com/bwmarrin/discordgo v0.23.2
-	github.com/codemicro/alib-go v0.0.2 // indirect
+	github.com/codemicro/alib-go v0.0.4
 	github.com/codemicro/dgo-toolkit v0.5.1
 	github.com/codemicro/dishook v0.0.0-20210103015922-e45eb21ab48b
 	github.com/gorilla/websocket v1.4.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.0
-	github.com/magefile/mage v1.11.0 // indirect
+	github.com/magefile/mage v1.11.0
 	github.com/mattn/go-sqlite3 v1.14.6 // indirect
 	github.com/mmcdole/gofeed v1.1.0
 	github.com/patrickmn/go-cache v2.1.0+incompatible
@@ -19,7 +19,7 @@ require (
 	github.com/tinylib/msgp v1.1.5
 	github.com/wcharczuk/go-chart/v2 v2.1.0
 	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392 // indirect
-	golang.org/x/sys v0.0.0-20201202213521-69691e467435 // indirect
+	golang.org/x/sys v0.0.0-20210324051608-47abb6519492 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.20.11
--- magefile.go
+++ magefile.go
@@ -20,6 +20,11 @@ import (
 
 var Default = Build
 
+const (
+	dockerImageTag      = "lgballtdiscordbot"
+	builtExecutableName = "lgballtDiscordBot"
+)
+
 func Build() error {
 	mg.Deps(InstallDeps)
 	mg.Deps(PreBuild)
@@ -32,21 +37,38 @@ func Build() error {
 		fileExtension = ".exe"
 	}
 
-	cmd := exec.Command("go", "build", "-o", path.Join("build", "lgballtDiscordBot" + fileExtension), "github.com/codemicro/lgballtDiscordBot/cmd/lgballtDiscordBot")
+	cmd := exsh.Command("go", "build", "-o", path.Join("build", builtExecutableName+fileExtension), "github.com/codemicro/lgballtDiscordBot/cmd/lgballtDiscordBot")
 	return cmd.Run()
 }
 
 func InstallDeps() error {
 	fmt.Println("Installing dependencies")
-	cmd := exec.Command("go", "mod", "download")
-	return cmd.Run()
+	cmd := exsh.Command("go", "mod", "download")
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	if !exsh.IsCmdAvail("gocloc") {
+		fmt.Println("Installing gocloc")
+		cmd = exsh.Command("go", "get", "-u", "github.com/hhatto/gocloc/cmd/gocloc")
+		cmd.Env = append(os.Environ(), "GO111MODULE=off")
+		if err := cmd.Run(); err != nil {
+			return err
+		}
+	} else {
+		if mg.Verbose() {
+			fmt.Println("Skipping gocloc install (found in PATH)")
+		}
+	}
+
+	return nil
 }
 
 func PreBuild() error {
 	fmt.Println("Running prebuild tasks")
 
 	if !exsh.IsCmdAvail("gocloc") {
-		return errors.New("gocloc must be installed on your system path - see https://github.com/hhatto/gocloc")
+		return errors.New("gocloc must be installed on your PATH - run `mage installDeps` or see https://github.com/hhatto/gocloc")
 	}
 
 	{
@@ -79,12 +101,12 @@ type Docker mg.Namespace
 
 func (Docker) Build() error {
 	if !exsh.IsCmdAvail("docker") {
-		return errors.New("docker must be installed on your system path - see https://docs.docker.com/get-docker/")
+		return errors.New("docker must be installed on your PATH - see https://docs.docker.com/get-docker/")
 	}
 
 	fmt.Println("Building Docker image")
 
 	// docker build . --file Dockerfile --tag $IMAGE_NAME
-	cmd := exec.Command("docker", "build", ".", "--file", "Dockerfile", "--tag", "lgballtdiscordbot")
+	cmd := exsh.Command("docker", "build", ".", "--file", "Dockerfile", "--tag", dockerImageTag)
 	return cmd.Run()
 }
