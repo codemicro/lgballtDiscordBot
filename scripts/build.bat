6949df246126e864bb3db903639bb79a69da0412
AKP <tom@tdpain.net>
2021-04-18 19:41:28 +0100

    Remove useless build scripts
    Signed-off-by: AKP <tom@tdpain.net>

--- scripts/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-go generate github.com/codemicro/lgballtDiscordBot/...
-go build github.com/codemicro/lgballtDiscordBot/cmd/lgballtDiscordBot
--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-go generate github.com/codemicro/lgballtDiscordBot/...
-go build github.com/codemicro/lgballtDiscordBot/cmd/lgballtDiscordBot
