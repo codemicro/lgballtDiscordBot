2d5184e09b824166eaa5a6030c420f2e484e034c
AKU <tom@tdpain.net>
2022-01-22 14:52:49 +0000

    Fix tone tag listing hitting embed length limit
    Signed-off-by: AKU <tom@tdpain.net>

--- internal/bot/components/toneTags/commands.go
+++ internal/bot/components/toneTags/commands.go
@@ -1,10 +1,12 @@
 package toneTags
 
 import (
+	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"github.com/codemicro/dgo-toolkit/route"
 	"github.com/codemicro/lgballtDiscordBot/internal/db"
 	"strings"
+	"time"
 )
 
 func sanitiseShorthand(tag string) string {
@@ -51,47 +53,68 @@ func (*ToneTags) List(ctx *route.MessageContext) error {
 		return err
 	}
 
-	div := len(all)/2
+	const (
+		zeroWidthSpace   = "​" // yes, THERE IS SOMETHING IN THAT STRING
+		embedTitle       = "Tone tag list"
+		embedDescription = "Tone tags are indicators that can be added to your message to help others in understanding your intended tone as a way to counteract the lacking nature of text-based communication. Text lacks certain audio and physical cues present in spoken speech (such as voice inflection, body language, facial expressions, etc.), so tone tags are a way to try and counteract that.\n\nHere's a list of them :P"
+	)
 
-	var sba strings.Builder
-	var sbb strings.Builder
+	var (
+		embeds            []*discordgo.MessageEmbed
+		currentEmbed      *discordgo.MessageEmbed
+		currentFieldIndex int
 
-	for i, tt := range all {
-		var ts *strings.Builder
-		if i < div {
-			ts = &sba
-		} else {
-			ts = &sbb
+		newEmbed = func() {
+			embeds = append(embeds, &discordgo.MessageEmbed{
+				Title:       embedTitle,
+				Description: embedDescription,
+				Fields:      []*discordgo.MessageEmbedField{},
+			})
+			currentEmbed = embeds[len(embeds)-1]
 		}
-		ts.WriteString("**/")
-		ts.WriteString(tt.Shorthand)
-		ts.WriteString("** - ")
-		ts.WriteString(tt.Description)
-		ts.WriteRune('\n')
+
+		newField = func() {
+			currentEmbed.Fields = append(currentEmbed.Fields, &discordgo.MessageEmbedField{
+				Name:   zeroWidthSpace,
+				Value:  "",
+				Inline: true,
+			})
+			currentFieldIndex = len(currentEmbed.Fields) - 1
+		}
+	)
+
+	newEmbed()
+	newField()
+
+	for _, tt := range all {
+		line := fmt.Sprintf("**/%s** - %s", tt.Shorthand, tt.Description)
+
+		// if the current field is going to be full (when the field length hits 1024, it trips up Discord, but here we
+		// have the limit set a little bit lower to make the embed more readable)
+		if len(currentEmbed.Fields[currentFieldIndex].Value)+len(line)+1 >= 512 { // plus one for the \n
+			// if no more fields should be added, so we make a new embed then add fields to that
+			if len(currentEmbed.Fields) >= 2 {
+				newEmbed()
+			}
+
+			newField()
+		}
+
+		currentEmbed.Fields[currentFieldIndex].Value += "\n" + line
 	}
 
-	const zeroWidthSpace = "​" // yes, THERE IS SOMETHING IN THAT STRING
-
-	emb := &discordgo.MessageEmbed{
-		Title:       "Tone tag list",
-		Description: "Tone tags are indicators that can be added to your message to help others in understanding your intended tone as a way to counteract the lacking nature of text-based communication. Text lacks certain audio and physical cues present in spoken speech (such as voice inflection, body language, facial expressions, etc.), so tone tags are a way to try and counteract that.\n\nHere's a list of them :P",
-		Fields: []*discordgo.MessageEmbedField{
-			{
-				// this ZWSP is needed since Discord requires something in embed field names - I can't just leave it blank
-				Name: zeroWidthSpace,
-				Value:  strings.TrimSpace(sba.String()),
-				Inline: true,
-			},
-			{
-				Name: zeroWidthSpace,
-				Value:  strings.TrimSpace(sbb.String()),
-				Inline: true,
-			},
-		},
+	if len(embeds) == 1 {
+		_, err = ctx.SendMessageEmbed(ctx.Message.ChannelID, embeds[0])
+		return err
 	}
 
-	_, err = ctx.SendMessageEmbed(ctx.Message.ChannelID, emb)
-	return err
+	for i, embed := range embeds {
+		embed.Footer = &discordgo.MessageEmbedFooter{
+			Text: fmt.Sprintf("Page %d of %d", i+1, len(embeds)),
+		}
+	}
+
+	return ctx.Kit.NewPaginate(ctx.Message.ChannelID, ctx.Message.Author.ID, embeds, time.Minute*10)
 }
 
 func (*ToneTags) Create(ctx *route.MessageContext) error {
@@ -100,8 +123,6 @@ func (*ToneTags) Create(ctx *route.MessageContext) error {
 	tag = sanitiseShorthand(tag)
 
 	desc := ctx.Arguments["description"].(string)
-
-	// save in DB
 
 	dbTag := &db.ToneTag{
 		Shorthand: tag,
